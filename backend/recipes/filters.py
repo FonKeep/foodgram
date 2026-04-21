00f9a2036cf5ad097447779a4e8551674b5239d3
Tikhon Karpukhin <fonkeep@mail.ru>
2026-04-21 21:58:18 +0300

    1

--- backend/recipes/filters.py
+++ backend/recipes/filters.py
@@ -1,16 +1,17 @@
+from django_filters import rest_framework as django_filters
+from rest_framework import filters
+
 from .models import Recipe, Tag
 
-from django_filters import rest_framework as filters
 
-
-class RecipeFilter(filters.FilterSet):
-    tags = filters.ModelMultipleChoiceFilter(
+class RecipeFilter(django_filters.FilterSet):
+    tags = django_filters.ModelMultipleChoiceFilter(
         field_name='tags__slug',
         to_field_name='slug',
         queryset=Tag.objects.all(),
     )
-    is_favorited = filters.BooleanFilter(method='filter_is_favorited')
-    is_in_shopping_cart = filters.BooleanFilter(
+    is_favorited = django_filters.BooleanFilter(method='filter_is_favorited')
+    is_in_shopping_cart = django_filters.BooleanFilter(
         method='filter_is_in_shopping_cart'
     )
 
@@ -19,15 +20,15 @@ class RecipeFilter(filters.FilterSet):
         fields = ('author', 'tags', 'is_favorited', 'is_in_shopping_cart')
 
     def filter_is_favorited(self, queryset, name, value):
-        if value and self.request.user.is_authenticated:
-            return queryset.filter(
-                favorited_by__user=self.request.user).distinct()
+        user = self.request.user
+        if value and user.is_authenticated:
+            return queryset.filter(favorite__user=user)
         return queryset
 
     def filter_is_in_shopping_cart(self, queryset, name, value):
-        if value and self.request.user.is_authenticated:
-            return queryset.filter(
-                in_shopping_cart__user=self.request.user).distinct()
+        user = self.request.user
+        if value and user.is_authenticated:
+            return queryset.filter(shopping_cart__user=user)
         return queryset
 
 
