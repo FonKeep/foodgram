9d18b3549d9f30d2f9e1f20ddab6739d31f7b6f8
Tikhon Karpukhin <fonkeep@mail.ru>
2026-04-21 22:28:25 +0300

    1

--- backend/recipes/filters.py
+++ backend/recipes/filters.py
@@ -1,18 +1,17 @@
-from django_filters import rest_framework as django_filters
+from django_filters.rest_framework import (BooleanFilter, FilterSet,
+                                           ModelMultipleChoiceFilter)
 from rest_framework.filters import SearchFilter
 
 from .models import Recipe, Tag
 
-class RecipeFilter(django_filters.FilterSet):
-    tags = django_filters.ModelMultipleChoiceFilter(
+class RecipeFilter(FilterSet):
+    tags = ModelMultipleChoiceFilter(
         field_name='tags__slug',
         to_field_name='slug',
         queryset=Tag.objects.all(),
     )
-    is_favorited = django_filters.BooleanFilter(method='filter_is_favorited')
-    is_in_shopping_cart = django_filters.BooleanFilter(
-        method='filter_is_in_shopping_cart'
-    )
+    is_favorited = BooleanFilter(method='filter_is_favorited')
+    is_in_shopping_cart = BooleanFilter(method='filter_is_in_shopping_cart')
 
     class Meta:
         model = Recipe
